7a9328564f05cd1391a17d316df5d0dfe07547ed
Steve Hill <code@seesteve.online>
2023-04-29 20:50:32 +0100

    Render grid

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Element as Ui
+import Element.Border as Border
 import Html exposing (Html)
 
 
@@ -11,11 +12,19 @@ main =
 
 
 type alias Model =
-    Cell
+    Grid
+
+
+type alias Grid =
+    List Cell
 
 
 type alias Cell =
-    { floor : CellFloor, ceiling : CellCeiling }
+    { coord : CellCoord, floor : CellFloor, ceiling : CellCeiling }
+
+
+type alias CellCoord =
+    ( Int, Int )
 
 
 type CellFloor
@@ -33,32 +42,64 @@ type Msg
     = NoOp
 
 
+gridHeight : Int
+gridHeight =
+    2
+
+
 init : Model
 init =
-    Cell (BombCount 2) Uncovered
+    [ Cell ( 1, 1 ) Bomb Covered
+    , Cell ( 1, 2 ) Bomb Uncovered
+    , Cell ( 1, 3 ) Bomb Flagged
+    , Cell ( 2, 1 ) (BombCount 2) Covered
+    , Cell ( 2, 2 ) (BombCount 3) Flagged
+    , Cell ( 2, 3 ) (BombCount 0) Uncovered
+    ]
 
 
 view : Model -> Html Msg
 view model =
-    Ui.layout [] <| viewCell model
+    Ui.layout [] <| viewGrid model
+
+
+viewGrid : Grid -> Ui.Element Msg
+viewGrid grid =
+    let
+        viewRow row =
+            grid
+                |> List.filter (\cell -> Tuple.first cell.coord == row)
+                |> List.map viewCell
+                |> Ui.row [ Ui.spacing 5 ]
+    in
+    List.range 1 gridHeight
+        |> List.map viewRow
+        |> Ui.column [ Ui.spacing 5 ]
 
 
 viewCell : Cell -> Ui.Element Msg
 viewCell { floor, ceiling } =
-    Ui.el [] <|
-        Ui.text <|
-            case ( floor, ceiling ) of
-                ( _, Covered ) ->
-                    "C"
+    Ui.el
+        [ Border.solid
+        , Border.width 1
+        , Ui.width <| Ui.px 50
+        , Ui.height <| Ui.px 50
+        ]
+    <|
+        Ui.el [ Ui.centerX, Ui.centerY ] <|
+            Ui.text <|
+                case ( floor, ceiling ) of
+                    ( _, Covered ) ->
+                        "C"
 
-                ( _, Flagged ) ->
-                    "F"
+                    ( _, Flagged ) ->
+                        "F"
 
-                ( Bomb, _ ) ->
-                    "B"
+                    ( Bomb, Uncovered ) ->
+                        "B"
 
-                ( BombCount bc, _ ) ->
-                    String.fromInt bc
+                    ( BombCount bombCount, Uncovered ) ->
+                        String.fromInt bombCount
 
 
 update : Msg -> Model -> Model
